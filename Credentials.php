80d17eda5304c876f7412bb1e0b243d222bd4cd0
Tiago Bilou <tiago.bilou@gmail.com>
2013-03-18 14:44:56 +0000

    Added a file to store the credentials

---
+++ Credentials.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Credentials
+ *
+ * @author tbilou
+ */
+class Credentials {
+
+
+
+}
+
+?>
